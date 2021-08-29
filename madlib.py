d893616cec5327b735b4f4c8d11e0fa48014183c
OmarJ <omarjandali93@gmail.com>
2021-08-29 10:33:53 -0700

    Compelted the mad lib assignment for ASC1100

---
+++ madlib.py
@@ -0,0 +1,34 @@
+
+# Homework: Create a madlib. Imagine a story where some of the words are 
+# supplied by user input. Using python you will use input to collect 
+# words for a story and then display the story. 
+
+# Use input to collect each word to a variable
+# Use an f string to display the story
+
+# Your madlib must collect at least 6 words!
+
+"----------------------------------------------"
+
+adj_1 = input("Give me and adjective: ")
+noun_1 = input("Give me a noun: ")
+verb_1 = input("Give me a past tense verb: ")
+adj_2 = input("Give me an adjective: ")
+noun_2 = input("Give me a noun: ")
+noun_3 = input("Give me a noun: ")
+adj_3 = input("Give me an adjective: ")
+verb_2 = input("Give me a verb: ")
+verb_3 = input("Give me a past tense verb: ")
+adj_4 = input("Give me an adjective: ")
+
+print("Today I went to the zoo. I saw a(n) " + adj_1 + " " + noun_1 + "jumping up and down in its tree. ")
+print("He " + verb_1 + "through the large tunnel that led to its " + adj_2 + " " + noun_2 + ".")
+print("I got some peanuts and passed them thought the cage to the gigantic grey " + noun_3 + "towering above my head. ")
+print("Feeding that animal made me hungry. ")
+print("I went to get a " + adj_3 + " scoop of ice cream. ")
+print("It filled my stomach.")
+print("Afterwards I had to " + verb_2 + " to catch our bus. ")
+print("When I got home I " + verb_3 + " my mom for a " + adj_4 + " day at the zoo. ")
+
+# --------------------------------------------------
+# Partial solution
